a8adb1c0af18e6f80dfea8e076f3050ab966bf9c
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2024-07-14 17:36:46 +0200

    Login
    Designed the login page and created the script for the login

---
+++ FrontEnd/scripts/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => { // Charge totalement le contenu du site avant d'executer le script
+    const token = localStorage.getItem('token'); // Je récupère le token stocké dans le localStorage
+    const loginMenuItem = document.querySelector('nav ul li:nth-child(3)'); // Je récupère le 3ème élément de ma nav (le bouton login)
+    
+    if (token) { 
+        // Utilisateur connecté
+        loginMenuItem.textContent = 'Logout'; // Je change le texte du bouton login en Logout
+        loginMenuItem.addEventListener('click', () => { // J'ajoute un event 'click' sur le bouton
+            localStorage.removeItem('token'); // Je supprime le token
+            window.location.reload(); // Je reload la page
+        });
+    } else { 
+        // Utilisateur non connecté
+        loginMenuItem.textContent = 'Login'; // J'écris login, si l'utilisateur n'est pas connecté, pour m'assurer de ne pas laisser logout par erreur
+        loginMenuItem.addEventListener('click', () => { // J'ajoute un event 'click' sur le bouton
+            window.location.href = 'login.html';  // Je redirige l'utilisateur vers la page de connexion
+        });
+    }
+});
